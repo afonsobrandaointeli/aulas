6a5f6ee940f84c329e4b29de428d73434f26b24b
afonsobrandaointeli <141183213+afonsobrandaointeli@users.noreply.github.com>
2024-05-24 11:00:09 -0300

    ko

--- config/env/production.js
+++ config/env/production.js
@@ -242,7 +242,7 @@ module.exports = {
      * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
      *                                                                          *
      ***************************************************************************/
-    onlyAllowOrigins: ['All'],
+    onlyAllowOrigins: ['http://*', 'https://*'],
     /***************************************************************************
      *                                                                          *
      * If you are deploying a cluster of multiple servers and/or processes,     *
