c1e814e8ccc3a79bafc27a04a58c1e205852fd57
afonsolelis <afonsolelis@gmail.com>
2024-05-31 00:09:23 -0300

    feat: aula de cicd

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,13 @@
 module.exports.routes = {
   "/": { view: "pages/homepage" },
+
+  // IN
   "/mod2/frontend3": { view: "pages/mod2/frontend3" },
+
+  // ES
+  "/mod10es/cicd": { view: "pages/mod10es/cicd" },
+  "/mod10es/performance": { view: "pages/mod10es/performance" },
+  "/mod10es/cloud": { view: "pages/mod10es/cloud" },
+  "/mod10es/sre": { view: "pages/mod10es/sre" },
+  "/mod10es/skills": { view: "pages/mod10es/skills" },
 };
